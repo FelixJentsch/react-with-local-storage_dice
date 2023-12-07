4beb2ba8cdf3ddce8eb2a054eb5b774969f0caff
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-07 11:26:50 +0100

    challenge solved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./styles.css";
+import React, { useState, useEffect } from "react";
+import { getD6Roll } from "./utils";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("dice-rolls");
+
+  useEffect(() => {
+    if (!rolls) {
+      // Initialize rolls with an empty array if it's not set
+      setRolls([]);
+    }
+  }, [rolls, setRolls]);
+
+  const handleRoll = () => {
+    const newRoll = { value: getD6Roll(), time: Date.now() };
+    setRolls((prevRolls) => [newRoll, ...prevRolls]);
+  };
+
+  const currentRollValue = rolls?.length > 0 ? rolls[0].value : null;
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls || []} />
       </aside>
     </div>
   );
